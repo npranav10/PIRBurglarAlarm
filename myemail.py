31fe3a17ee3f6251b31797006bc073225f8e5fff
Pranav N <npranav10@gmail.com>
2018-12-09 12:21:53 +0530

    Update myemail.py

--- myemail.py
+++ myemail.py
@@ -1,11 +1,13 @@
 import smtplib
-username = #Enter_your_Gmail ID
-password = #Enter_corresponding_password_for_you_mail_id
+username = #Enter your Gmail_ID in quotes as following
+# username =  "abc@gmail.com"
+password = #Enter corresponding_password for your mail_id in quotes as following
+# password = "Password"
 content = 'Intruder Alert !.......Mail sent form Raspberry Pi 3.0 using Python Script)'
 mail=smtplib.SMTP('smtp.gmail.com',587)  #Official Port for MSAs is 587 (Extended SMTP) . A variant of port 25 i.e standard SMTP
 mail.ehlo() # Tell Server that ESMTP is used instead of standard SMTP 
 mail.starttls() #Start Transport Layer Security
 mail.login(username,password)
-mail.sendmail('username','username',content)
+mail.sendmail(username,username,content)
 print("Mail Sent")
 mail.close()
